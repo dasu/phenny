aebb51d95d825303855a7ef85de46a26724b6395
dasu <desusp@gmail.com>
2013-12-20 12:27:44 -0500

    updated doge.py
    added currency denominations
    stop a possible exception
    added a price change since last check..up to 7 digits

--- modules/doge.py
+++ modules/doge.py
@@ -1,13 +1,19 @@
 #track dogecoin shit
 #ported from uguubot's implementation: 
 #https://github.com/infinitylabs/UguuBot/blob/e37d41ac92975d7506567133d32e973ba61f48fc/plugins/bitcoin.py
+#v0.47
+
 
 import re
 import json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen 
 
+lastPrice = 0
+
 def doge(phenny, input):
+	global lastPrice
+	go = 0
 	uri3 = 'http://data.mtgox.com/api/2/BTCUSD/money/ticker'
 	d = (urlopen(uri3).read()).decode('utf-8')
 	res = json.loads(d)
@@ -25,9 +31,16 @@ def doge(phenny, input):
 		dogerate = (re.search('.*last: (.*?\))\D', test).group(1))[:-1]
 	except:
 		phenny.say('doge is crashing or syrup is a bad bot, you decide')
-	
-	result = float(bitcoin_price) * float(dogerate)
-	dollar_result = 1 / float(result)
-	phenny.say("Current Value: \x0307$%s\x0f - 1 Doge = \x0307฿%s\x0f BTC - $1 = \x0307%s\x0f Doges" % (result,dogerate,dollar_result))
-doge.commands = ['doge','dogecoin','dc']
+		go = 1
 
+	if go is not 1:
+		result = float(bitcoin_price) * float(dogerate)
+		dollar_result = 1 / float(result)
+		diff = result - lastPrice
+		diffStr = ""
+		if diff != 0:
+			sign = "+" if diff > 0 else ''
+			diffStr = " (%s%s)" % (sign, format(diff,'.7f'))
+		phenny.say("Current Value: \x0307$%s%s\x0f - 1 Doge = \x0307฿%s\x0f BTC - $1 = \x0307Ɖ%s\x0f Doges" % (result,diffStr,dogerate,dollar_result))
+		lastPrice = result
+doge.commands = ['doge','dogecoin','dc']
