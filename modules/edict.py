bf358aa7403272aeae6f53f56ca14c791c4a74b4
dasu <desusp@gmail.com>
2014-09-25 16:53:49 -0400

    Update edict.py

--- modules/edict.py
+++ modules/edict.py
@@ -1,22 +1,33 @@
-#add kanji support
-
-from urllib.request import urlopen
+V0.8
+from urllib.request import urlopen, quote
 from bs4 import BeautifulSoup
 
 def edict(phenny, input):
-        if not input.group(2):
-                return phenny.say("Please enter a word.")
-        i = input.group(2)
-        print(i)
-        x = urlopen("http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi?1ZDJ%s"% i)
-        c = x.read()
-        bs = BeautifulSoup(c)
-        #print(bs)
-        if bs.pre:
-                res = bs.pre.contents[0].splitlines()[1]
-                #print(res)
-        else:
-                res = "No matches found."
-        phenny.say(res)
+	if not input.group(2):
+		return phenny.say("Please enter a word.")
+	i = input.group(2)
+	try:
+		i.encode('ascii')
+		print(i)
+		x = urlopen("http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi?1ZDJ%s"% i)
+		c = x.read()
+		bs = BeautifulSoup(c)
+		#print(bs)
+		if bs.pre:
+			res = bs.pre.contents[0].splitlines()[1]
+			#print(res)
+		else:
+			res = "No matches found."
+		phenny.say(res)
+	except:
+		print(i)
+		x = urlopen("http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi?1ZIK%s"%(quote(i)))
+		c = x.read()
+		bs = BeautifulSoup(c)
+		if bs.li:
+			res = bs.li.contents[0]
+		else:
+			res = "No matches found."
+		phenny.say(res)
 edict.commands = ['edict']
 edict.priority = 'medium'
